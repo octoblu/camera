a3c2af818dd3c6e2837cf9c7ee539d0b5d595654
virgilvox <moheeb.zara@citrix.com>
2015-11-15 20:11:21 -0700

    made watch better

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,32 +1,41 @@
+var app = angular.module('MyApp', ['ngMaterial']);
 
-var GET = {};
-var query = window.location.search.substring(1).split("&");
-for (var i = 0, max = query.length; i < max; i++)
-{
-  if (query[i] === "") // check for trailing & with no param
-  continue;
+app.controller('AppCtrl', function($scope) {
 
-  var param = query[i].split("=");
-  GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
-}
-var conn = meshblu.createConnection({
-  "uuid": GET.uuid,
-  "token": GET.token
-});
+  var GET = {};
+  var query = window.location.search.substring(1).split("&");
+  for (var i = 0, max = query.length; i < max; i++)
+  {
+    if (query[i] === "") // check for trailing & with no param
+    continue;
 
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+    var param = query[i].split("=");
+    GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
+  }
+  var conn = meshblu.createConnection({
+    "uuid": GET.uuid,
+    "token": GET.token
+  });
 
-conn.on('ready', function(data){
-  console.log('UUID AUTHENTICATED!');
 
-  conn.subscribe({uuid: GET.uuid, types: ["sent"]});
 
-  conn.on('message', function(data){
-    console.log('message received', data);
-    var img = new Image();
-    img.src = data.payload.pictures;
-    context.drawImage(img,0,0);
+  conn.on('ready', function(data){
+    console.log('UUID AUTHENTICATED!');
+    conn.subscribe({uuid: GET.uuid, types: ["sent"]});
+
+    conn.on('message', function(data){
+
+      var canvas = document.getElementById('canvas');
+      var context = canvas.getContext('2d');
+
+      var img = new Image();
+
+      img.onload = function() {
+        context.drawImage(this, 0, 0, canvas.width, canvas.height);
+      }
+      img.src = data.payload.pictures;
+
+    });
 
   });
 
